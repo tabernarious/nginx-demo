927bef6ed356702f132822a2eb4690ef42878757
Daniel Tavernier <tavdan@gmail.com>
2021-02-22 15:51:26 -0600

    Improving hit-count app

--- nginx-basic-lb-3/app1/app.py
+++ nginx-basic-lb-3/app1/app.py
@@ -2,13 +2,24 @@ from flask import Flask
 from redis import Redis
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 @app.route('/')
 
-def hello_world():
-  redis.incr('hits')
-  return 'Hello! This is App1 :)<br><br>Viewed %s times!' % redis.get('hits')
+def hello():
+  count = get_hit_count()
+  return 'Hello! This is App1 :)<br><br>Viewed {} times!'.format(count)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
