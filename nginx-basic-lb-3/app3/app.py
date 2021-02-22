9170e5a8c368613abecbb902f4960dec6c95adb0
Daniel Tavernier <tavdan@gmail.com>
2021-02-22 16:05:16 -0600

    Updating all apps with hit counter

--- nginx-basic-lb-3/app3/app.py
+++ nginx-basic-lb-3/app3/app.py
@@ -1,10 +1,26 @@
 from flask import Flask
+from redis import Redis
+import time
 
 app = Flask(__name__)
+cache = Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except cache.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
 @app.route('/')
 
-def hello_world():
-  return 'Hello! This is App3 :)'
+def hello():
+  count = get_hit_count()
+  return 'Hello! This is App3 :)<br><br>Viewed {} times!'.format(count)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
