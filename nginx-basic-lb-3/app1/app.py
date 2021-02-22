cab72f42ae922d6639a6795ff08550b6391670a2
Daniel Tavernier <tavdan@gmail.com>
2021-02-22 15:22:42 -0600

    Updated to incorporate redis

---
+++ nginx-basic-lb-3/app1/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import Redis
+
+app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
+
+@app.route('/')
+
+def hello_world():
+  redis.incr('hits')
+  return 'Hello! This is App1 :)\n\nViewed %s times!' % redis.get('hits')
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
