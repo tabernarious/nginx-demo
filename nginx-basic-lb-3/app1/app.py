24e6a8b172a7465ebd0f8fab2856b0ff222386a1
Daniel Tavernier <tavdan@gmail.com>
2021-02-23 15:52:50 -0600

    Reverted lb-3 demo to non-html templating

--- nginx-basic-lb-3/app1/app.py
+++ nginx-basic-lb-3/app1/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask
 from redis import Redis
 import time
-import os
 from datetime import datetime
 from datetime import timezone
 
@@ -20,27 +19,11 @@ def get_hit_count():
             time.sleep(0.5)
 
 @app.route('/')
+
 def hello():
   count = get_hit_count()
   dateTimeUTC = datetime.now(tz=timezone.utc)
-  return render_template(
-      'app.html',
-      appname = "App1",
-      count = count,
-      dateTimeUTC = dateTimeUTC
-  )
-
-@app.route('/favicon.ico')
-def fav():
-  return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico')
-
-@app.route('/favicon-16x16.png')
-def fav16():
-  return send_from_directory(os.path.join(app.root_path, 'static'),'favicon-16x16.png')
-
-@app.route('/favicon-32x32.png')
-def fav32():
-  return send_from_directory(os.path.join(app.root_path, 'static'),'favicon-32x32.png')
+  return 'Hello! This is App1 :)<br><br>Viewed {} times!<br><br>{} UTC'.format(count, dateTimeUTC)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
