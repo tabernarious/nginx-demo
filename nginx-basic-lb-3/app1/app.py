68e5437ff7d17a25f939e044a9edc01752cb1eb3
Daniel Tavernier <tavdan@gmail.com>
2021-02-23 15:25:37 -0600

    Additional favicons

--- nginx-basic-lb-3/app1/app.py
+++ nginx-basic-lb-3/app1/app.py
@@ -20,7 +20,6 @@ def get_hit_count():
             time.sleep(0.5)
 
 @app.route('/')
-
 def hello():
   count = get_hit_count()
   dateTimeUTC = datetime.now(tz=timezone.utc)
@@ -32,9 +31,16 @@ def hello():
   )
 
 @app.route('/favicon.ico')
-
 def fav():
   return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico')
+
+@app.route('/favicon-16x16.png')
+def fav():
+  return send_from_directory(os.path.join(app.root_path, 'static'),'favicon-16x16.png')
+
+@app.route('/favicon-32x32.png')
+def fav():
+  return send_from_directory(os.path.join(app.root_path, 'static'),'favicon-32x32.png')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
